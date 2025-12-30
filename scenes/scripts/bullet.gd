567f4c836f9ecf937e34db703541289ffea3dca5
Ansh Koshti <koshtiwork26@gmail.com>
2025-12-30 17:42:31 +0530

    Rocks explode function added with glitch fix.

--- scenes/scripts/bullet.gd
+++ scenes/scripts/bullet.gd
@@ -18,7 +18,7 @@ func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	queue_free()
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_bullet_body_entered(body: Node2D) -> void:
 	if body.is_in_group("rocks"):
-		# body_explode()
+		body.explode()
 		queue_free()
