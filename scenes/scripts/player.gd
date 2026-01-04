3ff5164048bb12010fbc5219bc85e430e1b4cdac
Ansh Koshti <koshtiwork26@gmail.com>
2026-01-04 18:52:20 +0530

    Ending the game function added.

--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -45,12 +45,19 @@ func change_state(new_state):
 	match new_state:
 		INIT:
 			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.modulate.a = 0.5
 		ALIVE:
 			$CollisionShape2D.set_deferred("disabled", false)
+			$Sprite2D.modulate.a = 1.0
 		INVULNERABLE:
 			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.modulate.a = 0.5
+			$InvulnerabilityTimer.start()
 		DEAD:
 			$CollisionShape2D.set_deferred("disabled", true)
+			$Sprite2D.hide()
+			linear_velocity = Vector2.ZERO
+			dead.emit()
 	state = new_state
 
 
@@ -92,4 +99,8 @@ func reset():
 	reset_pos = true
 	$Sprite2D.show()
 	lives = 3
+	change_state(ALIVE)
+
+
+func _on_invulnerability_timer_timeout() -> void:
 	change_state(ALIVE)
